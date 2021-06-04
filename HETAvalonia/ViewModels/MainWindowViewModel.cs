d6e950bf4121c3bc2ecd7436c8b595587ae2e270
mhinkle98 <44630605+mhinkle98@users.noreply.github.com>
2021-06-04 12:32:16 -0500

    Initial Commit

---
+++ HETAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HETAvalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+        public string SecondaryMessage => "Here is a second message";
+    }
+}
